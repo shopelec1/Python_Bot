e9fd3f0840cc6df9d0b309fdbfb38df0fd8835f3
shopelec1 <shopelec1@gmail.com>
2021-11-03 13:51:52 +0300

    //

---
+++ Python_Bot/Одноразовый_бот.py
@@ -0,0 +1,34 @@
+from telegram.ext import Updater
+updater = Updater(token='2065572942:AAF5bgFxR4phDBQXvwFI9fcQ9-F7eQs6sJM')
+dispatcher = updater.dispatcher
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
+
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+
+def admin_login_operation(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('admin_login_operation', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
+#ddd
+
+def unknown(update, context): #добавлять последним!
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Извините, эта команда мне ещё неизвестна")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
