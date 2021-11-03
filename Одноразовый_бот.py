2ce98e054a7041eb41e2a731e8a24d5ec20a305c
shopelec1 <shopelec1@gmail.com>
2021-11-03 15:33:30 +0300

    Добавлен скелет диалогов

--- Одноразовый_бот.py
+++ Одноразовый_бот.py
@@ -4,19 +4,14 @@ dispatcher = updater.dispatcher
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+context.bot.send_message(hat_id=update.effective_chat.id, text="Для начала отправь мне /start")
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Приветствую в магазине одноразовых электронных сигарет 'Одноразовый шоп'. Здесь можно узнать наличие (команда /stock ), оставить заявку на товар не из списка доступных (команда /ordernew ), а также узнать время работы сегодня (команда /worktime ). Приятных покупок! ")
 from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 updater.start_polling()
 
-
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-from telegram.ext import MessageHandler, Filters
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
 
 
 
@@ -26,11 +21,16 @@ from telegram.ext import CommandHandler
 admin_handler = CommandHandler('admin_enter_shop', admin_entry)
 dispatcher.add_handler(admin_handler)
 updater.start_polling()
-admin_login = Alibaba
-admin_password = Work
+admin_login = Alibaba ###
+admin_password = Work ###
+
+#from telegram.ext import MessageHandler, Filters
+#def login(update, context):
+
+#echo_handler = MessageHandler(Filters.text & (~Filters.command), login)
+#dispatcher.add_handler(echo_handler)
 
 def unknown(update, context): #добавлять последним!
     context.bot.send_message(chat_id=update.effective_chat.id, text="Извините, эта команда мне ещё неизвестна")
-
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
