61d56a6395164271fe5f69378c8e7a9eb2bad7eb
shopelec1 <shopelec1@gmail.com>
2021-11-03 17:02:40 +0300

    Улучшено приветствие

--- Одноразовый_бот.py
+++ Одноразовый_бот.py
@@ -8,7 +8,7 @@ def starter(update,context):
     context.send_message(сhat_id=update.effective_chat.id, text="Для начала отправь мне /start")
 starter
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Приветствую в магазине одноразовых электронных сигарет 'Одноразовый шоп'. Здесь можно узнать наличие (команда /stock ), оставить заявку на товар не из списка доступных (команда /ordernew ), а также узнать время работы сегодня (команда /worktime ). Приятных покупок! ")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Приветствую в магазине одноразовых электронных сигарет 'Одноразовый шоп'. Меня зовут Одноразовый Бот, приятно познакомиться. Здесь можно узнать наличие (команда /stock ), оставить заявку на товар не из списка доступных (команда /ordernew ), а также узнать время работы сегодня (команда /worktime ). Приятных покупок! ")
 from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
